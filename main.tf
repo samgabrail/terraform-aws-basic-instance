35c9e7a0347c50f217e1d2f6db38f76df1944f2b
Lance Haig <lnhaig@gmail.com>
2019-12-03 14:07:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,35 @@
+variable "prefix" {
+  default = "tfe"
+}
+
+variable "availability_zone" {
+  default = "eu-west-2a"
+}
+
+variable "aws_region" {
+  default = "eu-west-2"
+}
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = var.aws_region
+}
+
 resource "aws_vpc" "tfe_vpc" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "tfe-test"
+    Name = "${var.prefix}-test"
   }
 }
 
 resource "aws_subnet" "tfe_subnet" {
   vpc_id            = "${aws_vpc.tfe_vpc.id}"
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = var.availability_zone
 
   tags = {
-    Name = "tfe-test"
+    Name = "${var.prefix}-test"
   }
 }
 
