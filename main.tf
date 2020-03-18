d7b6d6ea4bed9a529546b23cc23728e8292e829b
Larry Eichenbaum <6776768+larryebaum@users.noreply.github.com>
2020-03-18 11:17:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_vpc" "tfe_vpc" {
 }
 
 resource "aws_subnet" "tfe_subnet" {
-  vpc_id            = aws_vpc.tfe_vpc.id
+  vpc_id            = "${aws_vpc.tfe_vpc.id}"
   cidr_block        = "172.16.10.0/24"
   availability_zone = var.availability_zone
 
@@ -33,24 +33,30 @@ resource "aws_subnet" "tfe_subnet" {
   }
 }
 
+resource "aws_network_interface" "web" {
+  subnet_id   = "${aws_subnet.tfe_subnet.id}"
+  private_ips = ["172.16.10.100"]
 
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
 
 resource "aws_instance" "web" {
   ami           = "ami-22b9a343" # us-west-2
   instance_type = "t2.micro"
-  associate_public_ip_address = true
 
   network_interface {
-    network_interface_id = aws_network_interface.web.id
+    network_interface_id = "${aws_network_interface.web.id}"
     device_index         = 0
   }
-
+  associate_public_ip_address = true
   credit_specification {
     cpu_credits = "unlimited"
   }
 }
 
-output "server_public_ip" {
-  value = aws_instance.web.public_ip
+output "server_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
   description = "The public IP address of the web server instance."
 }
