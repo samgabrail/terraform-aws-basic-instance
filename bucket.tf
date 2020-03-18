fa36c8f19b21b4ee53648080bf5f4da8e866ad4f
Larry Eichenbaum <6776768+larryebaum@users.noreply.github.com>
2020-03-18 12:51:56 -0400

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -6,6 +6,15 @@ resource "random_pet" "this" {
   length = 2
 }
 
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 1
+  tags = {
+    owner = var.prefix
+  }  
+ 
+}
+
 module "s3-bucket" {
   source     = "app.terraform.io/larryebaum-demo/s3-bucket/aws"
   version    = "1.6.0"
@@ -18,7 +27,7 @@ module "s3-bucket" {
   server_side_encryption_configuration = {
     rule = {
       apply_server_side_encryption_by_default = {
-        kms_master_key_id = aws_kms_key.objects.arn
+        kms_master_key_id = aws_kms_key.mykey.arn
         sse_algorithm     = "aws:kms"
       }
     }
