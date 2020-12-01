ade56a3fec07d50043464b2517145278b8b0241a
Larry Eichenbaum <6776768+larryebaum@users.noreply.github.com>
2020-12-01 14:50:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,57 +1,49 @@
-variable "prefix" {
-  default = "tfe"
+terraform {
+  required_version = ">= 0.11.0"
 }
 
-variable "availability_zone" {
-  default = "us-west-2a"
+provider aws {
+  region = var.aws_region
 }
 
-variable "aws_region" {
-  default = "us-west-2"
-}
-
-provider "aws" {
-  version = "~> 2.0"
-  region  = var.aws_region
+locals {
+  common_tags = {
+    owner = var.owner
+    se-region = var.se_region
+    purpose = var.purpose
+    ttl = var.ttl #-1 must has justification as purpose
+    terraform = var.terraform
+    creator = var.name
+    customer = var.customer
+    tfe-workspace = var.tfe_workspace
+    lifecycle-action = var.lifecycle_action
+    Name = "${var.owner}-{var.purpose}-{var.customer}" 
+  }
 }
 
 resource "aws_vpc" "tfe_vpc" {
   cidr_block = "172.16.0.0/16"
-
-  tags = {
-    Name = "${var.prefix}-test"
-    owner = var.prefix
-  }
+  tags = local.common_tags
 }
 
 resource "aws_subnet" "tfe_subnet" {
   vpc_id            = aws_vpc.tfe_vpc.id
   cidr_block        = "172.16.10.0/24"
   availability_zone = var.availability_zone
-
-  tags = {
-    Name = "${var.prefix}-test"
-    owner = var.prefix
-  }
+  tags = local.common_tags
 }
 
 resource "aws_network_interface" "web" {
   subnet_id   = aws_subnet.tfe_subnet.id
   private_ips = ["172.16.10.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-    owner = var.prefix
-  }
+  tags = local.common_tags
 }
 
 resource "aws_instance" "web" {
   ami           = "ami-22b9a343" # us-west-2
   instance_type = "t2.nano"
   associate_public_ip_address = true
-  tags = {
-  Name = "${var.prefix}-test"
-  owner = var.prefix
+  tags = local.common_tags
   }
 }
 
